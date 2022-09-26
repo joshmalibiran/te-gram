2996e1769d4d15516f900ed4892eedb03f85f6f8
Joshua Malibiran <JoshuaM2018@gmail.com>
2022-09-26 11:59:33 -0400

    completed database

--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -1,13 +1,49 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, posts, comments, favorites;
 
 CREATE TABLE users (
 	user_id SERIAL,
 	username varchar(50) NOT NULL UNIQUE,
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
+	user_picture varchar(300),
+	email varchar(50) NOT NULL,
+	
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
+
+CREATE TABLE posts	(
+	post_id SERIAL,
+	user_id integer NOT NULL,
+	post_picture varchar(300) NOT NULL,
+	caption varchar(1000) NOT NULL,
+	likes integer NOT NULL,
+	rating integer NOT NULL,
+
+	CONSTRAINT PK_post PRIMARY KEY(post_id),
+	CONSTRAINT FK_posts_users FOREIGN KEY(user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE comments	(
+	comment_id SERIAL,
+	post_id integer NOT NULL,
+	user_id integer NOT NULL,
+	comment_description varchar(1000) NOT NULL,
+	
+	CONSTRAINT PK_comments PRIMARY KEY(comment_id),
+	CONSTRAINT FK_comments_posts FOREIGN KEY(post_id) REFERENCES posts(post_id),
+	CONSTRAINT FK_comments_users FOREIGN KEY(user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE favorites	(
+	user_id integer,
+	post_id integer,
+
+	CONSTRAINT PK_favorites PRIMARY KEY(user_id, post_id),
+	CONSTRAINT FK_favorites_users FOREIGN KEY(user_id) REFERENCES users(user_id),
+	CONSTRAINT FK_favorites_posts FOREIGN KEY(post_id) REFERENCES posts(post_id)
+);
+
 
 COMMIT TRANSACTION;
