5f54ebd521a2bce15f7f6baa8dc0085582581657
Joshua Malibiran <JoshuaM2018@gmail.com>
2022-09-26 14:00:59 -0400

    updated user to include email and profile pic

--- capstone/java/src/test/resources/test-data.sql
+++ capstone/java/src/test/resources/test-data.sql
@@ -1,7 +1,7 @@
 BEGIN TRANSACTION;
 
-INSERT INTO users (username,password_hash,role) VALUES ('user1','user1','ROLE_USER');
-INSERT INTO users (username,password_hash,role) VALUES ('user2','user2','ROLE_USER');
-INSERT INTO users (username,password_hash,role) VALUES ('user3','user3','ROLE_USER');
+INSERT INTO users (username,password_hash,role,email,user_picture) VALUES ('user1','user1','ROLE_USER','email','');
+INSERT INTO users (username,password_hash,role,email,user_picture) VALUES ('user2','user2','ROLE_USER','email','');
+INSERT INTO users (username,password_hash,role,email,user_picture) VALUES ('user3','user3','ROLE_USER','email','');
 
 COMMIT TRANSACTION;
