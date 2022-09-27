6eac4d87207e43a2dccfe36d38459d7c6f4382ba
Joshua Malibiran <JoshuaM2018@gmail.com>
2022-09-27 10:00:59 -0400

    comment testing progress

--- capstone/java/src/test/java/com/techelevator/dao/JdbcCommentDaoTests.java
+++ capstone/java/src/test/java/com/techelevator/dao/JdbcCommentDaoTests.java
@@ -4,6 +4,8 @@ import com.techelevator.model.Comment;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.common.DataValidationException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.time.LocalDateTime;
@@ -11,6 +13,7 @@ import java.time.LocalDateTime;
 
 public class JdbcCommentDaoTests extends BaseDaoTests{
     protected static final Comment comment1 = new Comment(1, 1, 1, "comment", LocalDateTime.now());
+    protected static final Comment comment2 = new Comment(1, 1, 1, null, LocalDateTime.now());
 
 
     private JdbcCommentDao sut;
@@ -25,6 +28,17 @@ public class JdbcCommentDaoTests extends BaseDaoTests{
     public void create_creates_a_comment()    {
         boolean wasCreated = sut.create(comment1);
         Assert.assertTrue(wasCreated);
+    }
+
+    @Test(expected = DataIntegrityViolationException.class)
+    public void create_comment_with_null_description()    {
+        sut.create(comment2);
+    }
+
+    @Test
+    public void get_comment_by_comment_id() {
+        Comment returned = sut.getCommentByCommentId(1);
+        Assert.assertEquals(returned.getId(), 1);
     }
 
 }
--- capstone/java/src/test/resources/test-data.sql
+++ capstone/java/src/test/resources/test-data.sql
@@ -4,6 +4,12 @@ INSERT INTO users (username,password_hash,role,email,user_picture) VALUES ('user
 INSERT INTO users (username,password_hash,role,email,user_picture) VALUES ('user2','user2','ROLE_USER','email','');
 INSERT INTO users (username,password_hash,role,email,user_picture) VALUES ('user3','user3','ROLE_USER','email','');
 
-INSERT INTO posts(user_id, post_picture, caption, likes, date_posted) VALUES(1, 'pic', 'caption', 4, '2016-06-22 19:10:25-07');
+INSERT INTO posts(user_id, post_picture, caption, likes, date_posted) VALUES(1, 'pic', 'caption', 44, '2016-06-22 19:10:25-07');
+INSERT INTO posts(user_id, post_picture, caption, likes, date_posted) VALUES(2, 'pic', 'caption', 32, '2016-06-22 19:10:25-07');
+INSERT INTO posts(user_id, post_picture, caption, likes, date_posted) VALUES(3, 'pic', 'caption', 45, '2016-06-22 19:10:25-07');
+INSERT INTO posts(user_id, post_picture, caption, likes, date_posted) VALUES(1, 'pic', 'caption', 34, '2016-06-22 19:10:25-07');
 
+INSERT INTO comments(post_id, user_id, comment_description, date_posted) VALUES(1, 3, 'description', '2016-06-22 19:10:25-07');
+INSERT INTO comments(post_id, user_id, comment_description, date_posted) VALUES(1, 2, 'description', '2016-06-22 19:10:25-07');
+INSERT INTO comments(post_id, user_id, comment_description, date_posted) VALUES(3, 3, 'description', '2016-06-22 19:10:25-07');
 COMMIT TRANSACTION;
