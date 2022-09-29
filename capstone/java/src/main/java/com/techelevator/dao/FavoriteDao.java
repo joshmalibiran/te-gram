5fe73425a2d04528f90993fe25e7709dcd581e87
Brittany Ritter <Brittanylritter3@gmail.com>
2022-09-29 14:34:30 -0400

    Added the favorites in the backend

---
+++ capstone/java/src/main/java/com/techelevator/dao/FavoriteDao.java
@@ -0,0 +1,19 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.Favorite;
+import com.techelevator.model.Like;
+
+import java.util.List;
+
+public interface FavoriteDao {
+    //create
+    boolean create(int postId, int userId);
+
+    //delete
+    boolean delete(int postId);
+
+    //get likes
+    List<Favorite> getFavoritesFromUserId(int userId);
+
+
+}
