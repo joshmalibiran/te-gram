027182c6f0add2e6d89c55cf2be53b1bf3f79ee8
Katie Paulsen <paulsennkatie@gmail.com>
2022-09-30 10:52:36 -0400

    Reconfigured lightbox and styling for login/homepage

--- capstone/vue/src/components/Carousel.vue
+++ capstone/vue/src/components/Carousel.vue
@@ -3,36 +3,70 @@
     <!-- <img  src="../images/ezgif.com-gif-maker.gif" /> -->
     <div class="portfolio">
       <div class="portfolio_item">
-        <img src="https://unsplash.it/300?image=1080" alt="" />
+        <img src="https://unsplash.it/250?image=1080" alt="" />
         <div class="portfolio_desc"></div>
       </div>
 
       <div class="portfolio_item">
-        <img src="https://unsplash.it/300?image=100" alt="" />
+        <img src="https://unsplash.it/250?image=1070" alt="" />
         <div class="portfolio_desc"></div>
       </div>
 
       <div class="portfolio_item">
-        <img src="https://unsplash.it/300?image=1060" alt="" />
+        <img src="https://unsplash.it/250?image=1060" alt="" />
         <div class="portfolio_desc"></div>
       </div>
 
       <div class="portfolio_item">
-        <img src="https://unsplash.it/300?image=1020" alt="" />
+        <img src="https://unsplash.it/250?image=1020" alt="" />
         <div class="portfolio_desc"></div>
       </div>
-    </div>
 
     <!-- The portfolio lightboxes -->
     <div class="portfolio-lightboxes">
       <div class="portfolio-lightbox">
         <div class="portfolio-lightbox_content">
           <a href="#" class="close"></a>
-          <img src="https://unsplash.it/300?image=1080" alt="" />
+          <img src="https://unsplash.it/500/400?image=1080" alt="" />
           <h3 class="portfolio-lightbox_title">title in lightbox</h3>
           <p class="portfolio-lightbox_body">the content in lightbox</p>
         </div>
       </div>
+    </div>
+    
+    <div class="portfolio-lightboxes">
+      <div class="portfolio-lightbox">
+        <div class="portfolio-lightbox_content">
+          <a href="#" class="close"></a>
+          <img src="https://unsplash.it/500/400?image=1070" alt="" />
+          <h3 class="portfolio-lightbox_title">title in lightbox</h3>
+          <p class="portfolio-lightbox_body">the content in lightbox</p>
+        </div>
+      </div>
+    </div>
+    
+    <div class="portfolio-lightboxes">
+      <div class="portfolio-lightbox">
+        <div class="portfolio-lightbox_content">
+          <a href="#" class="close"></a>
+          <img src="https://unsplash.it/500/400?image=1060" alt="" />
+          <h3 class="portfolio-lightbox_title">title in lightbox</h3>
+          <p class="portfolio-lightbox_body">the content in lightbox</p>
+        </div>
+      </div>
+    </div>
+    
+    <div class="portfolio-lightboxes">
+      <div class="portfolio-lightbox">
+        <div class="portfolio-lightbox_content">
+          <a href="#" class="close"></a>
+          <img src="https://unsplash.it/500/400?image=1020" alt="" />
+          <h3 class="portfolio-lightbox_title">title in lightbox</h3>
+          <p class="portfolio-lightbox_body">the content in lightbox</p>
+        </div>
+      </div>
+    </div>
+    
     </div>
   </div>
 </template>
@@ -44,7 +78,35 @@ export default {
 </script>
 
 <style>
+img{
+  max-width: 100%;
+}
 .portfolio{
   height: 300px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1em;
+  padding: 1em;
+  background: rgb(207, 234, 255);
+}
+.portfolio_item{
+  
+}
+.portfolio-description{
+  padding: 1.5em;
+}
+.portfolio-title{
+  font-size: 1.5em;
+  margin-top: 0;
+}
+/* .portfolio-lightboxes {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100 vw;
+  height: 100 vh;
+} */
+.portfolio-lightboxes{
+  display: none;
 }
 </style>
