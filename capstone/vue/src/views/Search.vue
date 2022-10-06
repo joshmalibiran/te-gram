c6b9c1a68156ca88ccf3b857eacd695df513ade0
Katie Paulsen <paulsennkatie@gmail.com>
2022-10-06 10:04:41 -0400

    pushing updated search view

--- capstone/vue/src/views/Search.vue
+++ capstone/vue/src/views/Search.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
       <!-- <navigation-bar/> -->
-<top
+<top-nav-bar id="Topnavbar"/>
 <new-searchbar/>
   </div>
 </template>
@@ -10,15 +10,21 @@
 // import NavigationBar from '../components/NavigationBar.vue';
 
 import NewSearchbar from '../components/NewSearchbar.vue'
+import TopNavBar from '../components/TopNavBar.vue'
 
 export default {
     name: 'search',
-  components: { NewSearchbar,
-  // NavigationBar },
+  components: { NewSearchbar, TopNavBar
+   },
 
 };
 </script>
 
 <style>
-
+#Topnavbar{
+    display: flex;
+    align-items: center;
+    border-radius: 60px;
+    height: 100px;
+}
 </style>
